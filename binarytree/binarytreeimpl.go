eeb8303ff09038fedcab8950871704c80aea7f0b
Liam <liamwerner777@gmail.com>
2024-02-01 17:54:21 +0100

    implemented proper printing with correct spacing of the tree

--- binarytree/binarytreeimpl.go
+++ binarytree/binarytreeimpl.go
@@ -10,6 +10,7 @@ type tree struct {
 	root         *knot
 	current_knot *knot
 	knot_count   int
+	height       int
 }
 
 type knot struct {
@@ -48,19 +49,25 @@ func (t *tree) Insert(value int) {
 
 	if t.root == nil {
 		t.root = knot
+		t.height = 1
 		return
 	}
 
 	t.current_knot = t.root
-
+	count := 2
 	for {
 		if value <= t.current_knot.value && t.current_knot.leftPtr != nil {
 			t.current_knot = t.current_knot.leftPtr
+			count++
 		} else if value > t.current_knot.value && t.current_knot.rightPtr != nil {
 			t.current_knot = t.current_knot.rightPtr
+			count++
 		} else {
 			break
 		}
+	}
+	if count > t.height {
+		t.height = count
 	}
 
 	if value <= t.current_knot.value {
@@ -90,32 +97,56 @@ func (t *tree) Search(value int) *knot {
 	return knot
 }
 
-func (current_knot *knot) add_childs_of_knot_to_string(new_layer []*knot) (string, []*knot) {
+func (current_knot *knot) add_childs_of_knot_to_string(new_layer []*knot, layer_spacing int) (string, []*knot) {
 	var left_knot, right_knot string = "n", "n"
 	var string_addition string
 
-	if current_knot.leftPtr != nil {
-		left_knot = fmt.Sprint(current_knot.leftPtr.value)
+	if current_knot == nil {
+		new_layer = append(new_layer, nil, nil)
+		left_knot = " "
+		right_knot = " "
+
+	} else {
+
+		if current_knot.leftPtr != nil {
+			left_knot = fmt.Sprint(current_knot.leftPtr.value)
+		} else {
+			left_knot = " "
+		}
+
 		new_layer = append(new_layer, current_knot.leftPtr)
-	}
-	if current_knot.rightPtr != nil {
-		right_knot = fmt.Sprint(current_knot.rightPtr.value)
+
+		if current_knot.rightPtr != nil {
+			right_knot = fmt.Sprint(current_knot.rightPtr.value)
+
+		} else {
+			right_knot = " "
+		}
+
 		new_layer = append(new_layer, current_knot.rightPtr)
+
 	}
 
-	string_addition = left_knot + " " + right_knot + " "
+	var separator string = strings.Repeat(" ", layer_spacing)
+
+	string_addition = left_knot + separator + right_knot
 
 	return string_addition, new_layer
 }
 
-func interate_trough_layer(layer []*knot) (string, []*knot) {
+func interate_trough_layer(layer []*knot, layer_spacing int) (string, []*knot) {
 	var new_layer []*knot
 	var layer_string string
 	var layer_addition_string string
+	var separator string = strings.Repeat(" ", layer_spacing)
 
 	for i := 0; i < len(layer); i++ {
-		layer_addition_string, new_layer = layer[i].add_childs_of_knot_to_string(new_layer)
-		layer_string = layer_string + layer_addition_string
+		layer_addition_string, new_layer = layer[i].add_childs_of_knot_to_string(new_layer, layer_spacing)
+		if layer_string != "" {
+			layer_string = layer_string + separator + layer_addition_string
+		} else {
+			layer_string = layer_string + layer_addition_string
+		}
 	}
 	return layer_string, new_layer
 }
@@ -127,16 +158,39 @@ func (t *tree) Print(terminal_width int) string {
 	var current_layer []*knot
 	current_layer = append(current_layer, t.root)
 
+	var layer_count int = 1
+
 	for {
 		fmt.Println(centerString(string_addition, terminal_width))
+
+		layer_count++
 		s = s + string_addition + "\n"
-		string_addition, current_layer = interate_trough_layer(current_layer)
-		if len(current_layer) == 0 {
+		string_addition, current_layer = interate_trough_layer(current_layer, calculate_layer_spacing(t.height, layer_count))
+		if !layer_not_empty(current_layer) {
 			break
 		}
 	}
 
 	return s
+}
+
+func calculate_layer_spacing(tree_height, layer_count int) int {
+	var iteration_count int = tree_height - layer_count
+	var layer_spacing int = 1
+
+	for i := 0; i < iteration_count; i++ {
+		layer_spacing = (2 * layer_spacing) + 1
+	}
+	return layer_spacing
+}
+
+func layer_not_empty(layer []*knot) bool {
+	for i := 0; i < len(layer); i++ {
+		if layer[i] != nil {
+			return true
+		}
+	}
+	return false
 }
 
 func centerString(s string, width int) string {
