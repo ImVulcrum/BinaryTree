44c5b63b83c2725a249f564693a0c3dc4365db2e
Liam <>
2024-02-06 16:46:07 +0100

    implemeted proper way of displaying trees with multi digit values

--- binarytree.go
+++ binarytree.go
@@ -7,7 +7,7 @@ import (
 func main() {
 	var tree binarytree.Tree = binarytree.NewTree()
 
-	tree.InsertList([]int{7, 5, 3, 9, 4, 8, 2})
+	tree.InsertList([]int{7, 5, 3, 999999999, 4, 8, 2})
 
 	tree.Print(100)
 }
--- binarytree/binarytreeimpl.go
+++ binarytree/binarytreeimpl.go
@@ -2,15 +2,17 @@ package binarytree
 
 import (
 	"fmt"
+	"math"
 	"strings"
 )
 
 type tree struct {
-	knots        []*knot
-	root         *knot
-	current_knot *knot
-	knot_count   int
-	height       int
+	knots                   []*knot
+	root                    *knot
+	current_knot            *knot
+	knot_count              int
+	height                  int
+	max_len_of_value_string int
 }
 
 type knot struct {
@@ -46,6 +48,13 @@ func (t *tree) Insert(value int) {
 	var knot *knot = NewKnot(value)
 	t.knot_count++
 	t.knots = append(t.knots, knot)
+
+	if len(fmt.Sprint(value)) > t.max_len_of_value_string {
+		t.max_len_of_value_string = len(fmt.Sprint(value))
+		if t.max_len_of_value_string%2 == 0 {
+			t.max_len_of_value_string++
+		}
+	}
 
 	if t.root == nil {
 		t.root = knot
@@ -97,21 +106,22 @@ func (t *tree) Search(value int) *knot {
 	return knot
 }
 
-func (current_knot *knot) add_childs_of_knot_to_string(new_layer []*knot, layer_spacing int) (string, []*knot) {
+func (current_knot *knot) add_childs_of_knot_to_string(new_layer []*knot, layer_spacing int, max_len int) (string, []*knot) {
 	var left_knot, right_knot string = "n", "n"
 	var string_addition string
+	var separator string = strings.Repeat(" ", layer_spacing)
 
 	if current_knot == nil {
 		new_layer = append(new_layer, nil, nil)
-		left_knot = " "
-		right_knot = " "
+		left_knot = "n"
+		right_knot = "n"
 
 	} else {
 
 		if current_knot.leftPtr != nil {
 			left_knot = fmt.Sprint(current_knot.leftPtr.value)
 		} else {
-			left_knot = " "
+			left_knot = "n"
 		}
 
 		new_layer = append(new_layer, current_knot.leftPtr)
@@ -120,28 +130,39 @@ func (current_knot *knot) add_childs_of_knot_to_string(new_layer []*knot, layer_
 			right_knot = fmt.Sprint(current_knot.rightPtr.value)
 
 		} else {
-			right_knot = " "
+			right_knot = "n"
 		}
 
 		new_layer = append(new_layer, current_knot.rightPtr)
-
 	}
 
-	var separator string = strings.Repeat(" ", layer_spacing)
+	if len(left_knot)%2 == 0 {
+		left_knot = "0" + left_knot
+	}
+	if len(right_knot)%2 == 0 {
+		right_knot = "0" + right_knot
+	}
+
+	if len(left_knot) < max_len {
+		left_knot = equalize_different_value_lenghts(left_knot, max_len)
+	}
+	if len(right_knot) < max_len {
+		right_knot = equalize_different_value_lenghts(right_knot, max_len)
+	}
 
 	string_addition = left_knot + separator + right_knot
 
 	return string_addition, new_layer
 }
 
-func interate_trough_layer(layer []*knot, layer_spacing int) (string, []*knot) {
+func interate_trough_layer(layer []*knot, layer_spacing int, max_len_of_value_string int) (string, []*knot) {
 	var new_layer []*knot
 	var layer_string string
 	var layer_addition_string string
 	var separator string = strings.Repeat(" ", layer_spacing)
 
 	for i := 0; i < len(layer); i++ {
-		layer_addition_string, new_layer = layer[i].add_childs_of_knot_to_string(new_layer, layer_spacing)
+		layer_addition_string, new_layer = layer[i].add_childs_of_knot_to_string(new_layer, layer_spacing, max_len_of_value_string)
 		if layer_string != "" {
 			layer_string = layer_string + separator + layer_addition_string
 		} else {
@@ -153,57 +174,82 @@ func interate_trough_layer(layer []*knot, layer_spacing int) (string, []*knot) {
 
 func (t *tree) Print(terminal_width int) string {
 	var s string
-	var string_addition string = fmt.Sprint(t.root.value)
-
+	var string_addition string
 	var current_layer []*knot
-	current_layer = append(current_layer, t.root)
 
-	var layer_count int = 1
+	for layer_count := 0; true; layer_count++ {
+		if layer_count == 0 {
+			current_layer = append(current_layer, t.root)
+			string_addition = fmt.Sprint(t.root.value)
+			string_addition = equalize_different_value_lenghts(string_addition, t.max_len_of_value_string)
+		} else {
+			string_addition, current_layer = interate_trough_layer(current_layer, calculate_layer_spacing(t.height, layer_count+1, t.max_len_of_value_string), t.max_len_of_value_string)
+		}
 
-	for {
-		fmt.Println(centerString(string_addition, terminal_width))
-
-		layer_count++
-		s = s + string_addition + "\n"
-		string_addition, current_layer = interate_trough_layer(current_layer, calculate_layer_spacing(t.height, layer_count))
-		if !layer_not_empty(current_layer) {
+		if layer_empty(current_layer) {
 			break
 		}
+
+		string_addition = strings.Repeat(" ", calc_front_spacing(t.height, string_addition, t.max_len_of_value_string)) + string_addition
+		s = s + string_addition + "\n"
+
+		fmt.Println(string_addition)
 	}
 
 	return s
 }
 
-func calculate_layer_spacing(tree_height, layer_count int) int {
+func calculate_layer_spacing(tree_height, layer_count int, max_len_of_value_string int) int {
 	var iteration_count int = tree_height - layer_count
 	var layer_spacing int = 1
 
 	for i := 0; i < iteration_count; i++ {
-		layer_spacing = (2 * layer_spacing) + 1
+		layer_spacing = (2 * layer_spacing) + max_len_of_value_string
 	}
+
 	return layer_spacing
 }
 
-func layer_not_empty(layer []*knot) bool {
+func layer_empty(layer []*knot) bool {
 	for i := 0; i < len(layer); i++ {
 		if layer[i] != nil {
-			return true
+			return false
 		}
 	}
-	return false
+	return true
 }
 
-func centerString(s string, width int) string {
-	sLen := len(s)
-	if sLen >= width {
-		return s
+func calc_len_of_bottom_layer_string(tree_height int, max_len_of_value_string int) int {
+	if tree_height == 1 {
+		return 1
 	}
-
-	// Calculate the number of spaces needed on each side
-	padding := (width - sLen) / 2
-
-	// Build the centered string
-	centeredStr := strings.Repeat(" ", padding) + s + strings.Repeat(" ", padding)
-
-	return centeredStr
+	return max_len_of_value_string*int(math.Pow(2, float64(tree_height-1))) + int(math.Pow(2, float64(tree_height-1))) + 1
 }
+
+func calc_front_spacing(tree_height int, current_string string, max_len_of_value_string int) int {
+	var spacing int = (calc_len_of_bottom_layer_string(tree_height, max_len_of_value_string) - len(current_string)) / 2
+	if spacing < 0 {
+		return 0
+	}
+	return spacing
+}
+
+func equalize_different_value_lenghts(current_value_string string, max_string_size int) string {
+	var seperator string = strings.Repeat("-", (max_string_size-len(current_value_string))/2)
+	return seperator + current_value_string + seperator
+}
+
+// func centerString(s string, width int) string {
+// 	sLen := len(s)
+// 	if sLen >= width {
+// 		return s
+// 	}
+
+// 	// Calculate the number of spaces needed on each side
+// 	padding := (width - sLen) / 2
+
+// 	// Build the centered string
+// 	centeredStr := strings.Repeat(" ", padding) + s + strings.Repeat(" ", padding)
+
+// 	return centeredStr
+// }
