818db8676d158943064ab843d7c4152b09bca415
Liam <liamwerner777@gmail.com>
2024-02-18 12:22:25 +0100

    implemented avl trees

--- binarytree.go
+++ binarytree.go
@@ -7,10 +7,10 @@ import (
 )
 
 func main() {
-	var tree binarytree.Tree = binarytree.NewTree()
+	var tree binarytree.Tree = binarytree.NewTree(true)
 
-	//tree.InsertList([]int{7, 5, 3, 9, 4, 8, 2})
-	tree.InsertList([]int{26, 73, 91, 45, 60, 42, 32, 81, 84, 37, 43, 70, 94, 36, 51, 66, 19, 24, 99, 54, 75, 67, 88, 46, 56})
+	//clstree.InsertList([]int{7, 5, 3, 9, 4, 8, 2})
+	tree.InsertList([]int{67, 99, 20, 52, 55, 53, 80, 28, 14, 10, 51, 71, 16, 32, 73, 66, 98, 82, 21, 57, 34, 79, 40, 63, 94, 45, 25, 22, 84, 83})
 	//tree.InsertList([]int{62, 80, 50, 40, 70, 90, 60, 70, 30, 80, 59, 61, 89, 91})
 
 	fmt.Println(tree)
--- binarytree/binarytreeimpl.go
+++ binarytree/binarytreeimpl.go
@@ -9,15 +9,16 @@ type tree struct {
 	knots                   []*knot
 	root                    *knot
 	current_knot            *knot
-	knot_count              int
 	height                  int
 	max_len_of_value_string int
+	is_avl_tree             bool
 }
 
 type knot struct {
-	leftPtr  *knot
-	rightPtr *knot
-	value    int
+	leftPtr        *knot
+	rightPtr       *knot
+	value          int
+	balance_factor int
 }
 
 func NewKnot(value int) *knot {
@@ -28,13 +29,36 @@ func NewKnot(value int) *knot {
 	return k
 }
 
-func NewTree() *tree {
+func NewTree(is_avl_tree bool) *tree {
 	var t *tree = new(tree)
+	t.is_avl_tree = is_avl_tree
 	return t
 }
 
 func (t *tree) GiveKnots() []*knot {
 	return t.knots
+}
+
+func (t *tree) Search(value int) *knot {
+	return t.search(t.root, value)
+}
+
+func (t *tree) String() string {
+	var s string
+	var current_layer []*knot
+
+	for layer_count := 0; true; layer_count++ {
+		if layer_count == 0 {
+			current_layer = append(current_layer, t.root)
+		} else {
+			current_layer = interate_trough_layer(current_layer)
+		}
+		if layer_empty(current_layer) {
+			break
+		}
+		s = s + " " + create_layer_string(current_layer, t.height-layer_count, t.max_len_of_value_string) + "\n"
+	}
+	return s
 }
 
 func (t *tree) InsertList(values []int) {
@@ -44,9 +68,8 @@ func (t *tree) InsertList(values []int) {
 }
 
 func (t *tree) Insert(value int) {
-	var knot *knot = NewKnot(value)
-	t.knot_count++
-	t.knots = append(t.knots, knot)
+	var knot_to_be_inserted *knot = NewKnot(value)
+	t.knots = append(t.knots, knot_to_be_inserted)
 
 	if len(fmt.Sprint(value)) > t.max_len_of_value_string {
 		t.max_len_of_value_string = len(fmt.Sprint(value))
@@ -56,53 +79,141 @@ func (t *tree) Insert(value int) {
 	}
 
 	if t.root == nil {
-		t.root = knot
+		t.root = knot_to_be_inserted
 		t.height = 1
 		return
 	}
 
 	t.current_knot = t.root
-	count := 2
 	for {
 		if value <= t.current_knot.value && t.current_knot.leftPtr != nil {
 			t.current_knot = t.current_knot.leftPtr
-			count++
 		} else if value > t.current_knot.value && t.current_knot.rightPtr != nil {
 			t.current_knot = t.current_knot.rightPtr
-			count++
 		} else {
 			break
 		}
 	}
-	if count > t.height {
-		t.height = count
-	}
 
 	if value <= t.current_knot.value {
-		t.current_knot.leftPtr = knot
+		t.current_knot.leftPtr = knot_to_be_inserted
 	} else if value > t.current_knot.value {
-		t.current_knot.rightPtr = knot
+		t.current_knot.rightPtr = knot_to_be_inserted
+	}
+
+	if t.is_avl_tree {
+		var problem_knot *knot = t.calculate_balance_factors()
+		if problem_knot != nil { //rotate
+			t.rotate(problem_knot)
+		}
+	}
+	t.height = t.root.calculate_height_from_bottom()
+}
+
+func (t *tree) rotate(problem_knot *knot) {
+	root_of_problem_knot, problem_knot_is_left_child_of_root := t.get_previous_knot(t.root, problem_knot)
+
+	if problem_knot.balance_factor == 2 {
+		if problem_knot.rightPtr.balance_factor >= 0 { //simple left rotation
+			t.left_rotation(root_of_problem_knot, problem_knot, problem_knot_is_left_child_of_root)
+		} else { //right-left rotation
+			t.right_rotation(problem_knot, problem_knot.rightPtr, false)
+			t.left_rotation(root_of_problem_knot, problem_knot, problem_knot_is_left_child_of_root)
+		}
+	} else if problem_knot.balance_factor == -2 {
+		if problem_knot.leftPtr.balance_factor <= 0 { //simple right rotation
+			t.right_rotation(root_of_problem_knot, problem_knot, problem_knot_is_left_child_of_root)
+		} else { //left-right rotation
+			t.left_rotation(problem_knot, problem_knot.leftPtr, true)
+			t.right_rotation(root_of_problem_knot, problem_knot, problem_knot_is_left_child_of_root)
+		}
 	}
 }
 
-func (t *tree) search(value int) *knot {
-	if t.current_knot == nil {
-		return nil
-	} else if value < t.current_knot.value {
-		t.current_knot = t.current_knot.leftPtr
-		return t.search(value)
-	} else if value > t.current_knot.value {
-		t.current_knot = t.current_knot.rightPtr
-		return t.search(value)
+func (t *tree) left_rotation(root_of_problem_knot *knot, problem_knot *knot, problem_knot_is_on_the_left_of_root bool) {
+	var lower_knot *knot          //right_knot with balance factor 1 or 0
+	var subtree_to_be_moved *knot //middle knot
+
+	//save the knots
+	lower_knot = problem_knot.rightPtr
+	subtree_to_be_moved = lower_knot.leftPtr
+
+	//move the middle knot the the problem knot (with balance factor two)
+	problem_knot.rightPtr = subtree_to_be_moved
+
+	//add the problem knot to the left side of the lower knot
+	lower_knot.leftPtr = problem_knot
+
+	//add the lower knot the root
+	if root_of_problem_knot != nil { //the problem knot is not the root of the tree
+		if problem_knot_is_on_the_left_of_root {
+			root_of_problem_knot.leftPtr = lower_knot
+		} else {
+			root_of_problem_knot.rightPtr = lower_knot
+		}
+	} else { //the problem knot is the root of the tree
+		t.root = lower_knot
+	}
+}
+
+func (t *tree) right_rotation(root_of_problem_knot *knot, problem_knot *knot, problem_knot_is_on_the_left_of_root bool) {
+	var lower_knot *knot          //right_knot with balance factor -1 or 0
+	var subtree_to_be_moved *knot //middle knot
+
+	//save the knots
+	lower_knot = problem_knot.leftPtr
+	subtree_to_be_moved = lower_knot.rightPtr
+
+	//move the middle knot the the problem knot (with balance factor minus two)
+	problem_knot.leftPtr = subtree_to_be_moved
+
+	//add the problem knot to the left right of the lower knot
+	lower_knot.rightPtr = problem_knot
+
+	//add the lower knot the root
+	if root_of_problem_knot != nil { //the problem knot is not the root of the tree
+		if problem_knot_is_on_the_left_of_root {
+			root_of_problem_knot.leftPtr = lower_knot
+		} else {
+			root_of_problem_knot.rightPtr = lower_knot
+		}
+	} else { //the problem knot is the root of the tree
+		t.root = lower_knot
+	}
+}
+
+func (t *tree) get_previous_knot(relative_root *knot, knot_to_find_parent *knot) (*knot, bool) { //if the knot is the left child of the parent -> true; if it's the right -> false
+	if relative_root != nil {
+		if knot_to_find_parent.value < relative_root.value {
+			if relative_root.leftPtr == knot_to_find_parent {
+				return relative_root, true
+			} else {
+				return t.get_previous_knot(relative_root.leftPtr, knot_to_find_parent)
+			}
+		} else if knot_to_find_parent.value > relative_root.value {
+			if relative_root.rightPtr == knot_to_find_parent {
+				return relative_root, false
+			} else {
+				return t.get_previous_knot(relative_root.rightPtr, knot_to_find_parent)
+			}
+		} else { //this knot has no previous knot, therefore it's the root knot
+			return nil, false
+		}
 	} else {
-		return t.current_knot
+		panic("this function is broken")
 	}
 }
 
-func (t *tree) Search(value int) *knot {
-	t.current_knot = t.root
-	knot := t.search(value)
-	return knot
+func (t *tree) search(relative_root *knot, value int) *knot {
+	if relative_root == nil {
+		return nil
+	} else if value < relative_root.value {
+		return t.search(relative_root.leftPtr, value)
+	} else if value > relative_root.value {
+		return t.search(relative_root.rightPtr, value)
+	} else {
+		return relative_root
+	}
 }
 
 func (current_knot *knot) add_childs_of_knot_to_layer(new_layer []*knot) []*knot {
@@ -122,27 +233,6 @@ func interate_trough_layer(layer []*knot) []*knot {
 		new_layer = layer[i].add_childs_of_knot_to_layer(new_layer)
 	}
 	return new_layer
-}
-
-func (t *tree) String() string {
-	var s string
-	var current_layer []*knot
-
-	for layer_count := 0; true; layer_count++ {
-		if layer_count == 0 {
-			current_layer = append(current_layer, t.root)
-		} else {
-			current_layer = interate_trough_layer(current_layer)
-		}
-
-		if layer_empty(current_layer) {
-			break
-		}
-
-		s = s + " " + create_layer_string(current_layer, t.height-layer_count, t.max_len_of_value_string) + "\n"
-		//fmt.Println(" " + create_layer_string(current_layer, t.height-layer_count, t.max_len_of_value_string))
-	}
-	return s
 }
 
 func create_layer_string(layer []*knot, reverse_layer_count int, max_count_of_value_digits int) string {
@@ -262,29 +352,49 @@ func equalize_different_value_lenghts(current_value_string string, max_string_si
 	}
 }
 
-// func calc_len_of_bottom_layer_string(tree_height int, max_len_of_value_string int) int {
-// 	if tree_height == 1 {
-// 		return 1
-// 	}
-// 	return max_len_of_value_string*int(math.Pow(2, float64(tree_height-1))) + int(math.Pow(2, float64(tree_height-1))) + 1
-// }
+func (t *tree) calculate_balance_factors() *knot {
+	var lowest_problem_knot *knot
+	var current_knot *knot
 
-// func calc_front_spacing(tree_height int, current_string string, max_len_of_value_string int) int {
-// 	var spacing int = (calc_len_of_bottom_layer_string(tree_height, max_len_of_value_string) - len(current_string)) / 2
-// 	if spacing < 0 {
-// 		return 0
-// 	}
-// 	return spacing
-// }
+	for i := 0; i < len(t.knots); i++ {
+		current_knot = t.knots[i]
+		current_knot.calculate_balance_factor()
+		if current_knot.balance_factor == 2 || current_knot.balance_factor == -2 {
+			if (lowest_problem_knot != nil && current_knot.calculate_distance_to_root(t.height) > lowest_problem_knot.calculate_distance_to_root(t.height)) || lowest_problem_knot == nil {
+				lowest_problem_knot = current_knot
+			}
+		}
+	}
+	return lowest_problem_knot
+}
 
-// func centerString(s string, width int) string {
-// 	sLen := len(s)
-// 	if sLen >= width {
-// 		return s
-// 	}
-// 	// Calculate the number of spaces needed on each side
-// 	padding := (width - sLen) / 2
-// 	// Build the centered string
-// 	centeredStr := strings.Repeat(" ", padding) + s + strings.Repeat(" ", padding)
-// 	return centeredStr
-// }
+func (k *knot) calculate_balance_factor() int {
+	if k == nil {
+		k.balance_factor = 0
+		return 0
+	} else {
+		var left_height int = k.leftPtr.calculate_height_from_bottom()
+		var right_height int = k.rightPtr.calculate_height_from_bottom()
+		k.balance_factor = right_height - left_height
+		return right_height - left_height
+	}
+}
+
+func (k *knot) calculate_distance_to_root(tree_height int) int {
+	return tree_height - k.calculate_height_from_bottom()
+}
+
+func (k *knot) calculate_height_from_bottom() int {
+	if k == nil {
+		return 0
+	} else {
+		var left_height int = k.leftPtr.calculate_height_from_bottom()
+		var right_height int = k.rightPtr.calculate_height_from_bottom()
+
+		if left_height > right_height {
+			return left_height + 1
+		} else {
+			return right_height + 1
+		}
+	}
+}
